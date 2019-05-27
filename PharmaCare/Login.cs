f4dc1c2d9b757ca922f44a36148dc61ac3bd1d64
MarkoKlo <klobucarmarko1@gmail.com>
2019-05-28 00:09:23 +0200

    Version 0.1
    -Start of the creation of Login
    
    Issues:
    
    -Connection to server SQL

--- PharmaCare/Login.cs
+++ PharmaCare/Login.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace PhramaCare
 {
@@ -14,12 +15,15 @@ namespace PhramaCare
     {
         public Form1()
         {
+            SqlConnection sql = new SqlConnection();
+            sql.ConnectionString = "server=31.147.204.119\PISERVER,1433;database=TeamE5_DB;uid=TeamE5_User;pwd=9Gz_+GX8";
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            SqlConnection sql = new SqlConnection("server=31.147.204.119\PISERVER,1433;database=TeamE5_DB;uid=TeamE5_User;pwd=9Gz_+GX8");
+            sql.Open();
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -44,12 +48,30 @@ namespace PhramaCare
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            SqlConnection sql = new SqlConnection();
+            sql.ConnectionString = "server=31.147.204.119\PISERVER,1433;database=TeamE5_DB;uid=TeamE5_User;pwd=9Gz_+GX8";
+            sql.Open();
+            string userid = textBox1.Text;
+            string password = textBox2.Text;
+            SqlCommand cmd = new SqlCommand("select userid,password from users where userid='" + textBox1.Text + "'and password'" + textBox2.Text + "'", sql);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                MessageBox.Show("Login success");
+                //System.Diagnostics.Process.Start();
+            }
+            else
+            {
+                MessageBox.Show("Invalid Login please check username and password");
+            }
+            sql.Close();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            Application.Exit();
         }
     }
 }
