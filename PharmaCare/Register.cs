6170c5506e11337ed918342f0c27b4b69e00e80a
Guillaume Teak <guillaumeteak@gmail.com>
2019-06-21 20:24:33 +0200

    Register Finished

--- PharmaCare/Register.cs
+++ PharmaCare/Register.cs
@@ -7,11 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+
 
 namespace PhramaCare
 {
     public partial class Register : Form
     {
+        
+        string ConnectionString = "Server = 31.147.204.119; "+"Database = TeamE5_DB; "+"uid = TeamE5_User; "+"pwd = 9Gz_+GX8";
+        
+       
+
         public Register()
         {
             InitializeComponent();
@@ -43,6 +50,80 @@ namespace PhramaCare
         }
 
         private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonRegister_Click(object sender, EventArgs e)
+        {
+
+            if (txtusername.Text == "" || txtPassword.Text == "")
+            {
+                MessageBox.Show("Please fill the Username and the Password !");
+            }
+            else if(txtPassword.Text != txtConfirmPasswd.Text)
+            {
+                MessageBox.Show("Passwords don't match !");
+            }
+            else
+            {
+                using (SqlConnection sqlCon = new SqlConnection(ConnectionString))
+                {
+
+
+
+                    sqlCon.Open();
+                    //..
+                    SqlCommand sqlCmd = new SqlCommand("UserAdd", sqlCon);
+                    sqlCmd.CommandType = CommandType.StoredProcedure;
+                    sqlCmd.Parameters.AddWithValue("@user_id", txtusername.Text.Trim()); // ALLOWS TO ADD USER ID ON DATABASE WEBDIIIP
+                    sqlCmd.Parameters.AddWithValue("@password", txtPassword.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@name", txtName.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@mail", txtMail.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@IDnumber", txtIDnumber.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@phonenumber", txtPhonenumber.Text.Trim());
+                    sqlCmd.ExecuteNonQuery(); // Executes a Transact-SQL statement against the connection and returns the number of rows affected.
+
+                    MessageBox.Show(" WELL PLAYED ! REGISTRATION SUCCESSFUL ! WELCOME TO PHARMACARE !");
+                    Clear();
+                }
+            }
+
+            
+        }
+
+
+        void Clear()
+        {
+            txtusername.Text = txtPhonenumber.Text = txtPassword.Text = txtName.Text = txtMail.Text = txtIDnumber.Text = txtConfirmPasswd.Text = "";
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
         {
 
         }
