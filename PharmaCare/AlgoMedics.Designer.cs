8759f1cc70bafcec1b1bdf3095e0e23e781dcb65
Guillaume Teak <guillaumeteak@gmail.com>
2019-06-25 15:54:02 +0200

    Algo done but lacks a button that goes to user profil

---
+++ PharmaCare/AlgoMedics.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace PhramaCare
+{
+    partial class AlgoMedics
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append;
+            this.comboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(64, 242);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(248, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(371, 90);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(339, 290);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // AlgoMedics
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "AlgoMedics";
+            this.Text = "AlgoMedics";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
---
+++ PharmaCare/AlgoMedics.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace PhramaCare
+{
+    public partial class AlgoMedics : Form
+    {
+
+        string ConnectionString = "Server = 31.147.204.119; " + "Database = TeamE5_DB; " + "uid = TeamE5_User; " + "pwd = 9Gz_+GX8";
+
+
+        public AlgoMedics()
+        {
+            InitializeComponent();
+            FillSymptoms();
+
+        }
+
+
+        void FillSymptoms()
+        {
+            using (SqlConnection sqlCon = new SqlConnection(ConnectionString))
+            {
+                string Query = "select condition from [TeamE5_DB].[dbo].[Drugs] GROUP  BY condition HAVING COUNT(*) > 1 ORDER BY condition ASC, COUNT(*) ASC ";
+                SqlCommand all_drugs = new SqlCommand(Query, sqlCon);
+                SqlDataReader myReader;
+
+                sqlCon.Open();
+                myReader = all_drugs.ExecuteReader();
+
+
+                while (myReader.Read())
+                {
+                    //string column_index = myReader["condition"].ToString(); // SO WE GET THE INDeX OF COLUMN CONDITION WHICH IS OUR SYMPTOM
+                    //int columnValue = Convert.ToInt32(myReader['condition']);
+                    string Symptom_name = myReader["condition"].ToString();
+                    comboBox1.Items.Add(Symptom_name);
+                }
+
+                sqlCon.Close();
+
+
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String s = comboBox1.Text;
+            MessageBox.Show(s);
+
+            using (SqlConnection sqlCon = new SqlConnection(ConnectionString))
+            {
+
+                string Query = "select * from [TeamE5_DB].[dbo].[Drugs] where condition='" + s + "' ORDER BY drugName, (CASE when rating= '10.0' THEN '9.0' when rating = '9.0' THEN '8.0' when rating = '8.0' THEN '7.0' when rating = '7.0' THEN '6.0' when rating = '6.0' THEN '5.0' when rating = '5.0' THEN '4.0' when rating = '4.0' THEN '3.0' when rating = '3.0' THEN '2.0' when rating = '2.0' THEN '1.0' when rating = '1.0' THEN '' END ) DESC ";
+
+                SqlCommand allowed_drugs = new SqlCommand(Query, sqlCon);
+                SqlDataReader myReader;
+
+                sqlCon.Open();
+                myReader = allowed_drugs.ExecuteReader();
+
+
+                while (myReader.Read())
+                {
+                    //string column_index = myReader["condition"].ToString(); // SO WE GET THE INDeX OF COLUMN CONDITION WHICH IS OUR SYMPTOM
+                    //int columnValue = Convert.ToInt32(myReader['condition']);
+                    string Symptom_name =" | " + myReader["Id"].ToString() + " | " + myReader["drugName"].ToString() +  " | " + myReader["rating"].ToString();
+                    listBox1.Items.Add(Symptom_name);
+                }
+
+                sqlCon.Close();
+
+
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
