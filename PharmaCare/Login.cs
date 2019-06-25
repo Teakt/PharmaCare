3f83219ac0d6e7c68180f2693b8ce890d7f661fe
MarkoKlo <klobucarmarko1@gmail.com>
2019-06-25 17:31:36 +0200

    Interface done Ah bah super , Redirection from login to MedicALgo done, text in ALgo DOne

--- PharmaCare/Login.cs
+++ PharmaCare/Login.cs
@@ -78,6 +78,7 @@ namespace PhramaCare
             if (dt.Rows.Count > 0)
             {
                 MessageBox.Show("Login success");
+                ShowForm(AlgoMedics.Instance);
                 //System.Diagnostics.Process.Start();
             }
             else
--- PharmaCare/Register.Designer.cs
+++ PharmaCare/Register.Designer.cs
@@ -50,6 +50,7 @@
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -280,12 +281,23 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Arial Rounded MT Bold", 26.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(211, 47);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(320, 40);
+            this.label12.TabIndex = 13;
+            this.label12.Text = "Registration Page";
+            // 
             // Register
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(1584, 861);
+            this.Controls.Add(this.label12);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.groupBox1);
             this.Name = "Register";
@@ -295,6 +307,7 @@
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -321,5 +334,6 @@
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label12;
     }
 }
